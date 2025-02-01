01c9ce46463a9893ac339a3accde8dc828194f8f
youyi <youyichannel@163.com>
2025-02-01 21:18:19 +0800

    build(ui): add user center and settings route

---
+++ src/pages/User/Center/index.tsx
@@ -0,0 +1,8 @@
+import { PageContainer } from '@ant-design/pro-components';
+import React from 'react';
+
+const UserCenter: React.FC = () => {
+  return <PageContainer>{/* 个人中心页 */}</PageContainer>;
+};
+
+export default UserCenter;
---
+++ src/pages/User/Settings/index.tsx
@@ -0,0 +1,8 @@
+import { PageContainer } from '@ant-design/pro-components';
+import React from 'react';
+
+const UserSettings: React.FC = () => {
+  return <PageContainer>{/* 个人设置页 */}</PageContainer>;
+};
+
+export default UserSettings;
